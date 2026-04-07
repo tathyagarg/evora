a86f2d4ae27a93c0dbf523ecb53b6572db1f2296
Tathya <coding.tathya@gmail.com>
2026-04-08 00:09:06 +0530

    hotfix

--- src/lib/components/Don.svelte
+++ src/lib/components/Don.svelte
@@ -8,8 +8,12 @@
 
 <Button>
   <div class="flex flex-col sm:flex-row gap-4 h-full">
-    <img src={img} alt={name} class="h-full object-cover content-center" />
-    <div class="col-start-2">
+    <img
+      src={img}
+      alt={name}
+      class="w-full sm:w-1/2 object-cover content-center"
+    />
+    <div class="flex-1">
       <h3 class="text-2xl font-semibold mb-2">{name}</h3>
       <p class="text-sm">{description}</p>
     </div>
